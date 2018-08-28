fcc2c7159b7208b3a6c9073c1df3296f3e41299b
ngadiono <ngadiono@djavaui.com>
2018-08-28 10:34:18 +0700

    + func remove notes

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,10 +1,55 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New Note'
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
 };
 
-module.exports.add = (a, b) => {
-  return a + b;
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
+
+var addNote = (title, body) => {
+  var notes = [];
+  var note = {
+    title,
+    body
+  }
+
+var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+}
+
+var getAll = () => {
+  console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+}
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+
+  return notes.length  !== filteredNotes.length;
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
